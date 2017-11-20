8cfb93aae32aa1fbdf62661509a8b9c90794a720
Adam J <adamjac@kth.se>
2017-11-20 14:10:53 +0100

    Check predicates without multiple rules finished

--- modelchecker.pl
+++ modelchecker.pl
@@ -1,7 +1,14 @@
+% Created by Adam Jacobs
+% November 2017
+
+% Check if a list is not empty
+notEmpty([_|_]).
+
 % Load model, initial state and formula from file.
 verify(Input) :-
   see(Input), read(T), read(L), read(S), read(F), seen,
   check(T, L, S, [], F).
+
 % check(T, L, S, U, F)
 % T - The transitions in form of adjacency lists
 % L - The labeling
@@ -13,16 +20,30 @@ verify(Input) :-
 %
 % (T,L), S |- F
 % U
-% To execute: consult(’your_file.pl’). verify(’input.txt’).
+% See tests folder for example input
+% To execute: consult(’modelchecker.pl’). verify(’input.txt’).
 % Literals
-%check(_, L, S, [], X) :- ...
-%check(_, L, S, [], neg(X)) :- ...
+check(_, L, S, _, X) :- member([S, Vars], L), member(X, Vars).
+check(_, L, S, _, neg(X)) :- member([S, Vars], L), \+ member(X, Vars).
 % And
-%check(T, L, S, [], and(F,G)) :- ...
+check(T, L, S, U, and(F,G)) :- check(T, L, S, U, F), check(T, L, S, U, G).
 % Or
+check(T, L, S, U, or(F,G)) :- check(T, L, S, U, F) ; check(T, L, S, U, G).
 % AX
+check(T, L, S, U, ax(F)) :-
+  member([S, NextStates], T), !,
+  forall(member(SN, NextStates), check(T, L, SN, U, F)).
 % EX
+check(T, L, S, U, ex(F)) :-
+  member([S, NextStates], T), !,
+  member(SN, NextStates), check(T, L, SN, U, F), !.
 % AG
+%check(_, _, S, U, ag(_)) :-
+%  member(S, U), !.
+%check(T, L, S, U, ag(F)) :-
+%  member([S, NextStates], T), !,
+
+
 % EG
 % EF
 % AF
